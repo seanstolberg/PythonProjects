18420da34b1c966767953505e75b63eb768fbefd
Sean Stolberg <seanstolberg@hotmail.com>
2023-10-23 19:57:45 -0700

    Add documentation and working unit tests

--- RemoteLinuxTool/Tests/test_remotescriptmanager.py
+++ RemoteLinuxTool/Tests/test_remotescriptmanager.py
@@ -1,38 +1,75 @@
-from pathlib import Path
 """Module providing unit test functionality for python"""
+
 import pytest
+from pathlib import Path
 from testautomationtools.remotescriptmanager import RemoteScriptManager
 
 @pytest.fixture
 def rsm():
     """
-    _summary_
+    Fixture to create a RemoteScriptManager instance for testing.
 
     Returns:
-        _type_: _description_
+        RemoteScriptManager: An instance of RemoteScriptManager.
     """
     test_config = str(Path.joinpath(Path(__file__).parent, "test_config.yml"))
     return RemoteScriptManager(test_config)
 
-def test_constructor_default_values(rsm):
+def test_constructor_default_values(rsm: RemoteScriptManager):
     """
-    _summary_
+    Test the constructor of the RemoteScriptManager with default values.
+
+    Args:
+        rsm (RemoteScriptManager): An instance of RemoteScriptManager.
+
+    Raises:
+        AssertionError: If any assertion fails.
     """
+    # Define expected values for attributes
     exp_user = "user1"
     exp_target = "myTarget"
     exp_scp_path = "myPath/scp.exe"
     exp_ssh_path = "myPath/ssh.exe"
-    exp_scripts_dir ="scripts"
+    exp_scripts_dir = "scripts"
     exp_scripts = ["GetResourceInfo.sh"]
-    exp_target_logs_to_save = ["/var/log/resourceInfoLog.txt", "/var/log/messages"]
+    exp_target_logs_to_save = [
+        "/var/log/resourceInfoLog.txt", "/var/log/messages"]
     exp_destination_log_dir = "logs"
-    
+
+    # Perform assertions to check if attributes match expected values
     assert isinstance(rsm, RemoteScriptManager)
     assert rsm.user == exp_user, f"Expected self.user to be {exp_user}, but was {rsm.user}"
+    assert rsm.target == exp_target, f"Expected self.target to be {exp_target}, but was {rsm.target}"
+    assert rsm.scp_path == exp_scp_path, f"Expected self.scp_path to be {exp_scp_path}, but was {rsm.scp_path}"
+    assert rsm.ssh_path == exp_ssh_path, f"Expected self.ssh_path to be {exp_ssh_path}, but was {rsm.ssh_path}"
+    assert rsm.scripts_dir == exp_scripts_dir, f"Expected self.scripts_dir to be {exp_scripts_dir}, but was {rsm.scripts_dir}"
+    assert_lists_are_equal(exp_scripts, rsm.scripts)
+    rsm.target_logs_to_save.sort()
+    exp_target_logs_to_save.sort()
+    assert_lists_are_equal(exp_target_logs_to_save, rsm.target_logs_to_save)
+    assert rsm.destination_log_dir == exp_destination_log_dir, f"Expected self.destination_log_dir to be {exp_destination_log_dir}, but was {rsm.destination_log_dir}"
 
 def test_constructor_null_user():
     """
-    _summary_
+    Test the constructor of the RemoteScriptManager with a null user configuration.
+
+    Raises:
+        ValueError: If RemoteScriptManager is instantiated with a None config.
     """
     with pytest.raises(ValueError):
         RemoteScriptManager(None)
+
+def assert_lists_are_equal(expList, actList):
+    """
+    Utility function to assert that two lists are equal, disregarding order.
+
+    Args:
+        expList (list): The expected list.
+        actList (list): The actual list.
+
+    Raises:
+        AssertionError: If the lists are not equal.
+    """
+    actList.sort()
+    expList.sort()
+    assert actList == expList, f"Expected list to be {expList}, but was {actList}"
--- RemoteLinuxTool/testautomationtools/remotescriptmanager.py
+++ RemoteLinuxTool/testautomationtools/remotescriptmanager.py
@@ -3,43 +3,50 @@ import os
 from pathlib import Path
 import yaml
 
-
 class RemoteScriptManager:
     """
+    This class implements methods to manage a workflow on remote Linux devices.
+
     Author: Sean Stolberg
     Date: 10/21/2023
 
     Description:
-    This class implements methods to support a basic workflow where 1 to many remote Linux devices exist
-    and the user needs to iterate on the following workflow.
+    This class provides functionality to interact with remote Linux devices, supporting the following workflow:
 
-    1. Copy one or more test scripts to the remote Linux device
-    2. Execute the test scripts on the remote Linux device
-    3. When the test scripts are done executing, collect the logs created by the scripts and
-       the system log from /var/log/messages
-    4. Create a summary of any error messages encountered
+    1. Copy one or more test scripts to the remote Linux device.
+    2. Execute the test scripts on the remote Linux device.
+    3. Collect the logs created by the scripts and the system log from /var/log/messages after script execution.
+    4. Create a summary of any error messages encountered.
 
     Important Assumptions:
 
-    * RemoteScriptManager using both ssh and scp and thus requires that password-less ssh is already setup between the client
-    running this tool and the remote device/machine.
+    - This class utilizes SSH and SCP, requiring password-less SSH setup between the client running this tool
+      and the remote device/machine.
 
     Scripts used:
 
-    1. GetResourceInfo.sh - Generates a summary (snapshot in time) of the memory and disk usage, 
-       along with the CPU load at the time the script was run
+    1. GetResourceInfo.sh - Generates a summary (snapshot in time) of memory and disk usage, 
+       along with the CPU load at the time the script was run.
 
-        Example output:
+    Example output:
 
-        2023-10-21 17:37:03 Memory Usage: 3208/3829MB (83.78%)
-        2023-10-21 17:37:03 Disk Usage: 43G/3.8GGB (/)
-        2023-10-21 17:37:03 CPU Load: 0.08
-
+    2023-10-21 17:37:03 Memory Usage: 3208/3829MB (83.78%)
+    2023-10-21 17:37:03 Disk Usage: 43G/3.8GB (/)
+    2023-10-21 17:37:03 CPU Load: 0.08
     """
+
     def __init__(self, config: str):
-        if (config is None or not isinstance(config, str) or config.strip() == ""):
-            raise ValueError(
-                "config must be a non-null or empty string value.")
+        """
+        Initialize the RemoteScriptManager instance.
+
+        Args:
+            config (str): The path to the configuration file in YAML format.
+
+        Raises:
+            ValueError: If the `config` is None, empty, or not a string.
+        """
+        if config is None or not isinstance(config, str) or config.strip() == "":
+            raise ValueError("config must be a non-null or empty string value.")
 
         with open(config, 'r') as f:
             this_config = yaml.safe_load(f)
@@ -53,36 +60,46 @@ class RemoteScriptManager:
         self.target_logs_to_save = this_config["log_config"]["target_logs_to_save"]
         self.destination_log_dir = this_config["log_config"]["destination_log_dir"]
         self.ssh_target = f"{self.user}@{self.target}"
-        self.base_script_dir_as_posix = Path.joinpath(
-             Path(f"/{self.user}"), self.scripts_dir).as_posix()
+        self.base_script_dir_as_posix = Path.joinpath(Path(f"/{self.user}"), self.scripts_dir).as_posix()
 
     def ensure_target_script_dir(self):
-        print(
-            f"Ensure target script directory {self.base_script_dir_as_posix} is created.")
-        subprocess.run(
-            [self.ssh_path, self.ssh_target, f"mkdir -p {self.base_script_dir_as_posix}"])
+        """
+        Ensure that the target script directory is created on the remote device.
+        """
+        print(f"Ensure target script directory {self.base_script_dir_as_posix} is created.")
+        subprocess.run([self.ssh_path, self.ssh_target, f"mkdir -p {self.base_script_dir_as_posix}"])
 
     def copy_scripts_to_target(self):
-        for script in self.scripts:            
+        """
+        Copy script files to the remote device.
+        """
+        for script in self.scripts:
             destScriptPath = f"{self.ssh_target}:{Path.joinpath(Path(self.base_script_dir_as_posix), script).as_posix()}"
-            localScriptPath = Path.joinpath(
-                Path(os.getcwd()), self.scripts_dir, script)
+            localScriptPath = Path.joinpath(Path(os.getcwd()), self.scripts_dir, script)
             print(f"Copying {localScriptPath} to destination {destScriptPath}")
             subprocess.run([self.scp_path, localScriptPath, destScriptPath])
 
     def make_scripts_executable(self):
+        """
+        Make all script files in the remote script directory executable.
+        """
         command = f"find {self.base_script_dir_as_posix} -type f -name \"*.sh\" -exec chmod +x {{}} \;"
-        print(
-            f"Making all scripts in {self.base_script_dir_as_posix} executable")
+        print(f"Making all scripts in {self.base_script_dir_as_posix} executable")
         subprocess.run([self.ssh_path, self.ssh_target, command])
 
     def run_scripts(self):
+        """
+        Execute the script files on the remote device.
+        """
         for script in self.scripts:
-            command = "./scripts/" + script
+            command = f"./{self.scripts_dir}/" + script
             print(f"Running command: {command}")
             subprocess.run([self.ssh_path, self.ssh_target, command])
 
     def pull_logs_from_target(self):
+        """
+        Pull log files from the remote device to the local directory.
+        """
         destinationPath = Path(self.destination_log_dir)
         if not os.path.exists(destinationPath):
             os.makedirs(destinationPath)
@@ -90,16 +107,15 @@ class RemoteScriptManager:
         for log in self.target_logs_to_save:
             print(f"Pulling logs from: {log}")
             fileName = log.split("/")[-1]
-            
-            subprocess.run(
-                [self.scp_path, f"{self.ssh_target}:{log}",  Path.joinpath(Path(self.destination_log_dir), fileName)])
+            subprocess.run([self.scp_path, f"{self.ssh_target}:{log}", Path.joinpath(Path(self.destination_log_dir), fileName)])
 
     def execute(self):
+        """
+        Execute the complete workflow, including directory setup, copying scripts, making them executable,
+        running scripts, and pulling logs.
+        """
         self.ensure_target_script_dir()
         self.copy_scripts_to_target()
         self.make_scripts_executable()
         self.run_scripts()
         self.pull_logs_from_target()
-
-this_rsm = RemoteScriptManager(r"C:\Users\seans\Documents\GitHub\PythonProjects\RemoteLinuxTool\Tests\test_config.yml")
-this_rsm.execute()
